d6604e7477960500cb7a2ec21283efc7648ebd08
blondiee1 <103431926+blondiee1@users.noreply.github.com>
2022-08-30 22:52:32 -0300

    se agrega nueva funcion

---
+++ src/ejerc3_2/Coche.java
@@ -0,0 +1,12 @@
+package ejerc3_2;
+
+public class Coche {
+    int puertas;
+
+    public void incrementarPuertas (int incremento){
+        puertas += incremento;
+    }
+
+
+}
+
---
+++ src/ejerc3_2/Main.java
@@ -0,0 +1,12 @@
+package ejerc3_2;
+
+public class Main {
+    public static void main(String[] args) {
+    Coche miCoche = new Coche();
+
+    miCoche.incrementarPuertas (1);
+        System.out.println(miCoche.puertas);
+    }
+
+
+}
