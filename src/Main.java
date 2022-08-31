07f1fde71edef152a030a8632174c161842cd113
blondiee1 <103431926+blondiee1@users.noreply.github.com>
2022-08-30 22:10:04 -0300

    se agrega funcion suma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int resultant = suma(a,b,c);
+
+
+        System.out.println(resultant);
     }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+
+
 }
